f9501b07998ef32ae00d7153f850c90355eb2780
andrejr971 <andre.souza64@fatec.sp.gov.br>
2020-06-23 13:52:31 -0300

    Back-end inicialidado

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmenstsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentdRepository = getCustomRepository(AppointmentRepository);
+  const appointments = await appointmentdRepository.find();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider_id, date } = req.body;
+
+    const parsedDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
